e3a341810f9e7520ce15e2dfbce71306a05ececf
Uboat46 <uboat46@hotmail.com>
2018-05-24 09:47:21 -0500

    added info section

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -2,18 +2,21 @@
   <div class="landing-page">
     <nav-bar/>
     <hero-banner/>
+    <info-bar/>
   </div>
 </template>
 
 <script>
 import NavBar from './global/NavBar'
 import Hero from './Hero'
+import Info from './Info'
 
 export default {
   name: 'LandingPage',
   components: {
     'hero-banner': Hero,
-    'nav-bar': NavBar
+    'nav-bar': NavBar,
+    'info-bar': Info
   }
 }
 </script>
